cd4c3c6e6a56a3c1adb6ea898a0cd0e7e5b02368
Schparky <3172830+Schparky@users.noreply.github.com>
2019-11-27 16:52:03 -0700

    ensure DB transaction is committed when no errors occur

--- application/actions/gql.go
+++ application/actions/gql.go
@@ -2,9 +2,12 @@ package actions
 
 import (
 	"context"
+	"errors"
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+	"github.com/silinternational/wecarry-api/domain"
 	"github.com/silinternational/wecarry-api/gqlgen"
 )
 
@@ -13,5 +16,19 @@ func GQLHandler(c buffalo.Context) error {
 	newCtx := context.WithValue(c.Request().Context(), "BuffaloContext", c)
 	h.ServeHTTP(c.Response(), c.Request().WithContext(newCtx))
 
+	if res, ok := c.Response().(*buffalo.Response); ok {
+		if res.Status < 200 || res.Status >= 400 {
+			return errors.New("non-success error code returned from gqlgen handler")
+		}
+	}
+
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.New("no transaction found")
+	}
+	if err := tx.TX.Commit(); err != nil {
+		domain.ErrLogger.Printf("database commit failed, %s", err)
+		return err
+	}
 	return nil
 }
