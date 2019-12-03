8bed58da44ad0569c25fb41b80ba89884ec154df
Schparky <3172830+Schparky@users.noreply.github.com>
2019-12-03 08:19:38 -0700

    Found that testing res.Status didn't work. gqlgen can't set the response code because it has no understanding of a buffalo.Response.

--- application/actions/gql.go
+++ application/actions/gql.go
@@ -2,33 +2,39 @@ package actions
 
 import (
 	"context"
-	"errors"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/handler"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
 	"github.com/silinternational/wecarry-api/domain"
 	"github.com/silinternational/wecarry-api/gqlgen"
+	"github.com/vektah/gqlparser/gqlerror"
 )
 
 func GQLHandler(c buffalo.Context) error {
-	h := handler.GraphQL(gqlgen.NewExecutableSchema(gqlgen.Config{Resolvers: &gqlgen.Resolver{}}))
+	gqlSuccess := true
+
+	h := handler.GraphQL(gqlgen.NewExecutableSchema(gqlgen.Config{Resolvers: &gqlgen.Resolver{}}),
+		handler.ErrorPresenter(
+			func(ctx context.Context, e error) *gqlerror.Error {
+				gqlSuccess = false
+				return graphql.DefaultErrorPresenter(ctx, e)
+			}))
+
 	newCtx := context.WithValue(c.Request().Context(), "BuffaloContext", c)
 	h.ServeHTTP(c.Response(), c.Request().WithContext(newCtx))
 
-	if res, ok := c.Response().(*buffalo.Response); ok {
-		if res.Status < 200 || res.Status >= 400 {
-			return nil
-		}
+	if !gqlSuccess {
+		return nil
 	}
 
 	tx, ok := c.Value("tx").(*pop.Connection)
 	if !ok {
-		return errors.New("no transaction found")
+		domain.ErrLogger.Printf("no transaction found in GQLHandler")
 	}
 	if err := tx.TX.Commit(); err != nil {
 		domain.ErrLogger.Printf("database commit failed, %s", err)
-		return err
 	}
 	return nil
 }
